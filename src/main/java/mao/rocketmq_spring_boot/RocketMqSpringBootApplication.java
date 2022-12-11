437e786e535d7b296e48588979f064d92873b11a
mao <1296193245@qq.com>
2022-12-11 18:28:12 +0800

    rocketMQ_spring_boot

---
+++ src/main/java/mao/rocketmq_spring_boot/RocketMqSpringBootApplication.java
@@ -0,0 +1,15 @@
+package mao.rocketmq_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketMqSpringBootApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RocketMqSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/rocketmq_spring_boot/RocketMqSpringBootApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.rocketmq_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqSpringBootApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
